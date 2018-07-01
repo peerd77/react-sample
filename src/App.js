95901b99e06d322aa3ba60775aaf54d1775d30ff
Daniel <peer77@gmail.com>
2018-07-01 13:33:44 +0300

    in the middle of lec 44

--- src/App.js
+++ src/App.js
@@ -2,30 +2,35 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 class App extends Component {
- state = {
-   persons : [
-     {name:"Max", age:"28"},
-     {name:"Manu", age:"29"},
-     {name:"Steph", age:"26"},
-    ],
- }
- switchNameHandler = () => {
-   this.setState({
+  state = {
     persons : [
-      {name:"Max", age:"29"},
-      {name:"Manu", age:"30"},
-      {name:"Steph", age:"27"},
-     ],
-  });
- }
+      {name:"Max", age:"28"},
+      {name:"Manu", age:"29"},
+      {name:"Steph", age:"26"},
+    ],
+  }
+  switchNameHandler = () => {
+    this.setState({
+      persons : [
+        {name:"Max", age:"29"},
+        {name:"Manu", age:"30"},
+        {name:"Steph", age:"27"},
+      ],
+    });
+  }
   render() {
     return (
       <div className="App">
-        <h1>Hi, I'm a react App</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      <h1>Hi, I'm a react App</h1>
+      <button onClick={this.switchNameHandler}>Switch Name</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+      <Person 
+      name={this.state.persons[1].name} 
+      age={this.state.persons[1].age}
+      click={this.switchNameHandler}>
+      <span>My Hobbies: Racing</span>
+      </Person>
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
