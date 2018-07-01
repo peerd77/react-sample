d0893d47abf3045f56d6a67e96ef6d8d1999a68e
Daniel <peer77@gmail.com>
2018-07-01 13:40:02 +0300

    in the end of lec 44

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ class App extends Component {
       {name:"Steph", age:"26"},
     ],
   }
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     this.setState({
       persons : [
-        {name:"Max", age:"29"},
+        {name:newName, age:"29"},
         {name:"Manu", age:"30"},
         {name:"Steph", age:"27"},
       ],
@@ -22,12 +22,12 @@ class App extends Component {
     return (
       <div className="App">
       <h1>Hi, I'm a react App</h1>
-      <button onClick={this.switchNameHandler}>Switch Name</button>
+      <button onClick={this.switchNameHandler.bind(this, 'Maximilian')}>Switch Name</button>
       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
       <Person 
       name={this.state.persons[1].name} 
       age={this.state.persons[1].age}
-      click={this.switchNameHandler}>
+      click={this.switchNameHandler.bind(this, 'Max!')}>
       <span>My Hobbies: Racing</span>
       </Person>
       <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
